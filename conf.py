b53c00098420f3414c90a703d87ebe89f72ab796
Ben Spaulding <benjamin.spaulding@lionbridge.com>
2015-03-13 08:46:44 -0600

    Update copyright year

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Lionbridge Content API Documentation'
-copyright = u'2014, Lionbridge Technologies'
+copyright = u'2015, Lionbridge Technologies'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
